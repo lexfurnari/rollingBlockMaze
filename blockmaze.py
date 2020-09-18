d22a75157ac07870e2fa2869fba44cdb13d0f39d
tzwu21 <58194793+tzwu21@users.noreply.github.com>
2020-09-18 15:11:25 -0400

    blockmaze.py

--- blockmaze.py
+++ blockmaze.py
@@ -20,7 +20,7 @@ class Node:
     def __hash__(self):
         return hash(self.state)
 
-
+#TEST CODE
 def up(state, location):
     if state is True:
         state = False
@@ -46,24 +46,70 @@ def check_state(state):
         return standing
     else:
         standing = False
-        return False
+        return standing
 
+
+#A* search
+def aStar(gn, hn, start, goal):
+    frontier =  list()
+    explored = list()
+    fn = gn + hn
+    s = Node(fn, None)
+    frontier.append(s)
+    while frontier:
+        frontier.sort()
+        x = frontier.pop(0)
+        if (x.state is goal):
+            return x.state
+        explored.append(x.state)
+        for i in range(hn):
+            child = Node(fn,x)
+            if child.state not in explored and child not in frontier:
+                frontier.append(child)
+            elif child in frontier:
+                frontier.append(child)
+
+
+    
+#retrieves maze file from command line argument
 mazeNum = sys.argv[1]
 
 def main():
+    fileCounter = 0
     movCounter = 0
     blockState = True
     position = None
     start = 0
+    x = list()
+    y = list()
     print(mazeNum)
     maze = open(mazeNum + ".txt", "r")
-    print(maze.read())
-    for i in maze:
+    print(maze.read())                          #prints full map of maze
+    maze = open(mazeNum + ".txt", "r")
+    mazeLines = maze.readlines()                #creates 2D array of maze
+    print(mazeLines)
+    maze = open(mazeNum + ".txt", "r")
+    for i in maze.read():                       #locates start position "S"
+        fileCounter = fileCounter + 1
+        if "S" in i:                            
+            print(fileCounter)
+    
+    #TEST CODE
+    print(mazeLines[0][0])                      #prints "S" for maze1
+    print(mazeLines[6][6])                      #prints "G" for maze2
+
+    maze.close
+
+'''
+    if "S" in line:
         print("Check")
         if maze[i] == "S":
             start = i
             print(start)
             break
+        '''
+
+    
 
 
 if __name__ == "__main__": 
